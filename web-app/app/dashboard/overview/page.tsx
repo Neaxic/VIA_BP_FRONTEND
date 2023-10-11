5829d7398c551a8e396822befffa305c704e91c5
Andre <andreas79001@gmail.com>
2023-10-11 18:29:35 +0200

    added basic graphs + styling

--- web-app/app/dashboard/overview/page.tsx
+++ web-app/app/dashboard/overview/page.tsx
@@ -1,15 +1,114 @@
 "use client"
 
-import { Button } from "../../../components/ui/button";
-import { usePageContext } from "../../contexts/PageContext";
+import { Card } from "../../../components/ui/card";
+import { Label } from "../../../components/ui/label";
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 
+const data = [
+    {
+        name: "Jan",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Feb",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Mar",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Apr",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "May",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Jun",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Jul",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Aug",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Sep",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Oct",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Nov",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+        name: "Dec",
+        total: Math.floor(Math.random() * 5000) + 1000,
+    },
+]
 
 export default function Page() {
-    const { getHints } = usePageContext()
 
     return (
         <>
             <h1>Hello, overview page!</h1>
-            <Button onClick={getHints}>Get Hints</Button>
+            <div className="flex w-full gap-4">
+
+                <Card className="w-full p-2">
+                    <Label className="mb-2 text-lg ml-2">Revenue</Label>
+                    <ResponsiveContainer width="100%" className="mt-4" height={350}>
+                        <BarChart data={data}>
+                            <XAxis
+                                dataKey="name"
+                                stroke="#888888"
+                                fontSize={12}
+                                tickLine={false}
+                                axisLine={false}
+                            />
+                            <YAxis
+                                stroke="#888888"
+                                fontSize={12}
+                                tickLine={false}
+                                axisLine={false}
+                                tickFormatter={(value) => `$${value}`}
+                            />
+                            <Tooltip labelClassName="color-primary" />
+                            <Bar dataKey="total" className="fill-primary" radius={[4, 4, 0, 0]} />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </Card>
+                <Card className="w-full p-2">
+                    <Label className="mb-2 text-lg ml-2">Revenue</Label>
+                    <ResponsiveContainer width="100%" className="mt-4" height={350}>
+                        <LineChart data={data}>
+                            <XAxis
+                                dataKey="name"
+                                stroke="#888888"
+                                fontSize={12}
+                                tickLine={false}
+                                axisLine={false}
+                            />
+                            <YAxis
+                                stroke="#888888"
+                                fontSize={12}
+                                tickLine={false}
+                                axisLine={false}
+                                tickFormatter={(value) => `$${value}`}
+                            />
+                            <Tooltip />
+                            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                            <Line type="monotone" dataKey="total" className="stroke-primary" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </Card>
+            </div>
         </>);
 }
