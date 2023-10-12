03e91944fb1d7570c0c73f44966855901a786fd0
Andre <andreas79001@gmail.com>
2023-10-12 21:29:36 +0200

    added additional UI for unauthed user

--- web-app/app/s/layout.tsx
+++ web-app/app/s/layout.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import { Label } from "../../components/ui/label";
+import { useRouter } from "next/navigation";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../../components/ui/alert-dialog";
 import { useUserContext } from "../contexts/UserContext";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
     children,
@@ -12,7 +14,16 @@ export default function RootLayout({
     //This is made to act as a proxy layer for everything under /s
     //Like the dashboard.
     //This is where we can do some auth checks, ect.
+    const [openDialog, setOpenDialog] = useState(false);
     const { isLoggedIn } = useUserContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        //Only on mount, fix hyrdation issues
+        setTimeout(() => {
+            setOpenDialog(true);
+        }, 100)
+    }, []);
 
     return (
         <>
@@ -20,7 +31,22 @@ export default function RootLayout({
                 { children }
             ) : (
                 <div className="">
-                    <Label>Not logged in</Label>
+                    <AlertDialog open={openDialog}>
+                        <AlertDialogContent open={true}>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>You do not have access</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This is a restricted, and protected url. You must be logged in to access this page.
+                                    Contact your administrator if you believe this is a mistake.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel onClick={() => router.push("/")}>Okay, i give up</AlertDialogCancel>
+                                <AlertDialogAction onClick={() => router.push("/authentication/login")}>Login
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
                 </div>
             )}
         </>
