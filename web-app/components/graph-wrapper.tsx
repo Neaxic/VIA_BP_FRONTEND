579a5b2a2a9c900f4ce41f005725bf80b8cecd1e
Andre <andreas79001@gmail.com>
2023-10-13 15:33:47 +0200

    More worh on graph wrapper w. settings

--- web-app/components/graph-wrapper.tsx
+++ web-app/components/graph-wrapper.tsx
@@ -1,8 +1,13 @@
 import * as React from "react"
 import { Label } from "./ui/label"
 import { Button } from "./ui/button"
-import { Settings } from "lucide-react"
+import { CalendarIcon, Settings } from "lucide-react"
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Calendar } from "./ui/calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { cn } from "../lib/utils"
+import { DateRange } from "react-day-picker"
+import { addDays, format } from "date-fns"
 
 interface graphWrapperProps {
     enableSettings?: boolean
@@ -10,6 +15,11 @@ interface graphWrapperProps {
 }
 
 export function GraphWrapper({ children, enableSettings = true, ...props }: graphWrapperProps) {
+    const [date, setDate] = React.useState<DateRange | undefined>({
+        from: new Date(2022, 0, 20),
+        to: addDays(new Date(2022, 0, 20), 20),
+    })
+
     return (
         <>
             <div className="flex justify-between p-3">
@@ -23,12 +33,52 @@ export function GraphWrapper({ children, enableSettings = true, ...props }: grap
                         </SheetTrigger>
                         <SheetContent>
                             <SheetHeader>
-                                <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+                                <SheetTitle>Power vs production graph</SheetTitle>
                                 <SheetDescription>
-                                    This action cannot be undone. This will permanently delete your account
-                                    and remove your data from our servers.
+                                    Conviently change settings for the graph in question.
                                 </SheetDescription>
                             </SheetHeader>
+                            <div className="pt-12">
+                                <Label className="p-1">Date interval</Label>
+                                <div className={"grid gap-2"}>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <Button
+                                                id="date"
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-[300px] justify-start text-left font-normal",
+                                                    !date && "text-muted-foreground"
+                                                )}
+                                            >
+                                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                                {date?.from ? (
+                                                    date.to ? (
+                                                        <>
+                                                            {format(date.from, "LLL dd, y")} -{" "}
+                                                            {format(date.to, "LLL dd, y")}
+                                                        </>
+                                                    ) : (
+                                                        format(date.from, "LLL dd, y")
+                                                    )
+                                                ) : (
+                                                    <span>Pick a date</span>
+                                                )}
+                                            </Button>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                initialFocus
+                                                mode="range"
+                                                defaultMonth={date?.from}
+                                                selected={date}
+                                                onSelect={setDate}
+                                                numberOfMonths={2}
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </div>
+                            </div>
                         </SheetContent>
                     </Sheet>
                 )}
