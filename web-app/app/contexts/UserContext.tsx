2583ca22324099fe7e56f8f43d94577c3b946eec
Andre <andreas79001@gmail.com>
2023-10-13 12:25:49 +0200

    added more user logic to context 4 saving loggedin state

--- web-app/app/contexts/UserContext.tsx
+++ web-app/app/contexts/UserContext.tsx
@@ -4,11 +4,13 @@ import * as React from 'react'
 interface UserContextInterface {
     isLoggedIn: boolean
     registerUser: (username: string, isAdmin: boolean) => void
+    signOut: () => void
 }
 
 export const UserContext = React.createContext<UserContextInterface>({
     isLoggedIn: false,
     registerUser: () => { },
+    signOut: () => { },
 })
 
 export default function UserProvider({ children, }: { children: React.ReactNode }) {
@@ -19,6 +21,24 @@ export default function UserProvider({ children, }: { children: React.ReactNode 
         //Lav et obj med userdata
         //Det her er til efter man er logget ind, og siden skal gemme datet fra servern.
         //Den giver os alt data vi skal bruge, og gemme i storen / contexten
+        savingDataToLocalStorage(username, isAdmin) //mere data her en dag
+    }
+
+    const savingDataToLocalStorage = (username: string, isAdmin: boolean) => {
+        localStorage.setItem("user", JSON.stringify({ username: username, isAdmin: isAdmin }))
+    }
+
+    const loadingDataFromLocalStorage = () => {
+        const data = localStorage.getItem("user")
+        if (data) {
+            const user = JSON.parse(data)
+            registerUser(user.username, user.isAdmin)
+        }
+    }
+
+    const signOut = () => {
+        localStorage.removeItem("user")
+        setIsLoggedIn(false)
     }
 
 
@@ -27,6 +47,7 @@ export default function UserProvider({ children, }: { children: React.ReactNode 
 
     React.useEffect(() => {
         //Vi skal have logik her der tjekke
+        loadingDataFromLocalStorage()
     }, [])
 
     return (
@@ -34,6 +55,7 @@ export default function UserProvider({ children, }: { children: React.ReactNode 
             value={{
                 isLoggedIn,
                 registerUser,
+                signOut,
             }}
         >
             {children}
