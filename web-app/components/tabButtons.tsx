9b55d633ddff30b2fea3963e9241ea26b8344d4e
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-11-08 11:27:12 +0100

    Ændret admin Lavet flere components

--- web-app/components/tabButtons.tsx
+++ web-app/components/tabButtons.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 
 const TabButtons = () => {
+  // State variable for at holde styr på, om knapperne er toggled eller ej.
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggled(!isToggled); // Skifter tilstanden
+  };
+
   return (
     <div className="tab-buttons-container">
-      <Button onClick={() => console.log("Handling for knap 1")}>Add</Button>
-      <Button onClick={() => console.log("Handling for knap 2")}>Edit</Button>
-      <Button onClick={() => console.log("Handling for knap 3")}>Remove</Button>
+      <Button onClick={() => console.log("Handling for Add")}>Add</Button>
+      {isToggled ? (
+        <>
+          <Button onClick={() => console.log("Handling for Save")}>Save</Button>
+          <Button onClick={() => console.log("Handling for Cancel")}>
+            Cancel
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button onClick={() => console.log("Handling for Remove")}>
+            Find
+          </Button>
+          <Button onClick={() => console.log("Handling for Edit")}>Edit</Button>
+          <Button onClick={() => console.log("Handling for Remove")}>
+            Remove
+          </Button>
+        </>
+      )}
+      <Button onClick={handleToggle}>
+        {isToggled ? "Edit/Remove" : "Save/Cancel"}
+      </Button>
     </div>
   );
 };
+
 export default TabButtons;
